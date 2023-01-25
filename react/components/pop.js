ebfbe86fc07a915a7be8c79d724c90aef4e8a420
README.md <xxxxxx@gmail.com>
2023-01-25 09:39:09 +0000

    A

---
+++ react/components/pop.js
@@ -0,0 +1,14 @@
+      <ul className={styles.pop_btn}>
+        <li>
+          <h2><a href='#'>Home</a></h2>
+        </li>
+        <li>
+          <h2><a href='#'>About</a></h2>
+        </li>
+        <li>
+          <h2><a href='#'>Topics</a></h2>
+        </li>
+        <li>
+          <h2><a href='#'>Link</a></h2>
+        </li>
+      </ul>
--- react/pages/index.js
+++ react/pages/index.js
@@ -1,9 +1,29 @@
-import Footer from 'components/footer'
+import Link from 'next/link'
 
 export default function Home () {
   return (
     <>
-      <p>CodePrep</p>
+      <ul>
+        <li>
+          <h2><a href='/'>Home</a></h2>
+        </li>
+        <li>
+          <h2><a href='/about'>Webページ</a></h2>
+        </li>
+        <li>
+          <h2><a href='/blog'>新聞</a></h2>
+        </li>
+        <li>
+          <h2><a href='/cube'>３段組</a></h2>
+        </li>
+        <li>
+          <h2><a href='/hero'>PC</a></h2>
+        </li>
+        <li>
+          <h2><a href='/nav'>ポップ</a></h2>
+        </li>
+      </ul>
+
     </>
   )
 }
